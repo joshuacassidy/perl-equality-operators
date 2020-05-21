3ae49427cd0e427e906f4f70aaf2e8f5775b0417
joshuacassidy <joshcassidy79@gmail.com>
2020-05-21 17:44:28 +0100

    initial commit

---
+++ equality_operators.pl
@@ -0,0 +1,19 @@
+
+print(1==1, "\n");
+print("3" eq "3", "\n");
+
+print(1 != 1, "\n");
+print("3" ne "3", "\n");
+
+print(1 < 1, "\n");
+print("3" lt "3", "\n");
+
+print(1 > 1, "\n");
+print("3" gt "3", "\n");
+
+print(1 >= 1, "\n");
+print("3" ge "3", "\n");
+
+print(1 <= 1, "\n");
+print("3" le "3", "\n");
+
